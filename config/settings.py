6a698a12cd9c87eb84e2d0aa72c817fb3568c53a
lokesh-tasks <kesulokesh5@gmail.com>
2026-06-27 22:45:12 +0530

    Updated AI Negotiation Marketplace

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ai_negotiation_marketplace',
+        'USER': 'root',
+        'PASSWORD': 'Sheeba123*',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
